8866995f4ccbdf783a100ce633e87de58c733149
KasparsLapsa <kaspars.lapsa999@gmail.com>
2020-02-25 20:21:12 +0200

    second comit for task 2

---
+++ Lesson6/src/Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 50;
+        int c = 40;
+        int sum = a+b+c;
+
+
+        System.out.println(sum);
+    }
+}
